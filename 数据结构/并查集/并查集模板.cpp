9f78bbf8d6edac78a09de949828af14f676bfeaa
IIIndex <1656281660@qq.com>
2025-08-22 22:10:36 +0800

    修改并查集模板

--- 数据结构/并查集/并查集模板.cpp
+++ 数据结构/并查集/并查集模板.cpp
@@ -1,17 +1,21 @@
-/*一定一定记得开始的时候初始化fa[i]=i*/
+namespace DSU {
 
-int fa[maxn];
+vector fa(0, 0);
 
-int find(int x){                                            //寻找x的集合“老大”
-	if(fa[x]==x)return x;
-	else return fa[x]=find(fa[x]);                          //顺便更新x一路上的，使其成为老大的直接下属
+void init(int n) {
+    fa = vector(n + 1, 0);
+    iota(all(fa), 0);
 }
 
-void unionn(int x,int y){                                   //将x和y置于同一集合
-	int fx=find(x),fy=find(y);
-	if(fx==fy)return;                                       //如果本来就在一个集合，直接返回
-	else fa[fx]=fy;                                         //这里可以优化，具体见下
+int find(int u) {
+    return u == fa[u] ? u : fa[u] = find(fa[u]);
 }
 
-/*在连接两个集合的时候，如果把小集合链接到大集合下面，可以减小以后查询的时候的更新时间，这就要用到辅助数组，辅助数组直接依附于集合最顶元素（也
-就是x=fa[x]的x，辅助数组的大小就是依附于x（包括x）的元素的个数*/
+int merge(int u, int v) {
+    u = find(u); v = find(v);
+    if (u == v) return 0;
+    fa[u] = v;
+    return 1;
+}
+
+}
